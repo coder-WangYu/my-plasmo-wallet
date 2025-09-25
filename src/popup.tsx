a7cf6c3922a3e8ef924e31aa5925005c3225cd63
wangyu <coder_wangyu@163.com>
2025-09-25 20:22:16 +0800

    create：添加index主页面

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
 import { Routes, Route, MemoryRouter } from "react-router-dom"
 import Login from "./pages/login"
+import Index from "./pages/index"
 
 function IndexPopup() {
   return (
     <MemoryRouter initialEntries={["/"]}>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </MemoryRouter>
   )
